108b1389f10ac8309fee0a7b72bb5110b9c302c4
Davide Bono <davidebono1605@gmail.com>
2025-02-22 17:31:45 +0100

    corretto errore di battitura nella migrazione announcements

--- database/migrations/2025_02_19_171602_create_announcements_table.php
+++ database/migrations/2025_02_19_171602_create_announcements_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2)->nullable();
-            $table->string('immagine')->nullable();
+            $table->string('img')->nullable();
 
             $table->timestamps();
 
